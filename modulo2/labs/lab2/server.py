d6f020be05830f64e476a6749c6e7e409d1a1a9e
Lorena Mamede <loremamede@gmail.com>
2022-05-04 10:17:51 -0300

    Adicionando comentários com explicações de cada passo

--- modulo2/labs/lab2/server.py
+++ modulo2/labs/lab2/server.py
@@ -24,23 +24,23 @@ while True: #fica em loop esperando pelo ativo
 	if not msg: break #trata o encerramento da conexao pelo lado ativo
 
 	try:
-		name = msg.decode("utf-8")
-		file = open(name, "r")
+		name = msg.decode("utf-8")   #decodifica a mensagem
+		file = open(name, "r")       #busca o arquivo
 		d = dict()
-		for line in file:
+		for line in file:            #percorre linha a linha do arquivo
 			line = line.lower()
 			line = line.strip()
-			words = line.split(" ")
-			for word in words:
-				if word in d and word != "":
+			words = line.split(" ") #trata a linha e transforma em palavras
+			for word in words:      #percorre palavra a palavra da linha
+				if word in d and word != "":   #se a palavra ja estiver no dicionario aumenta o numero de ocorrencias
 					d[word] = d[word]+1
 				else:
-					d[word] = 1
-		res = dict(sorted(d.items(), key=itemgetter(1), reverse=True)[:5])
-		encode_res = json.dumps(res, indent=2).encode('utf-8')
-		novoSock.send(encode_res)
+					d[word] = 1            #se nao tiver, cria uma entrada pela primeira vez
+		res = dict(sorted(d.items(), key=itemgetter(1), reverse=True)[:5]) #reordena o dicionario pelo numero de ocorrencia e retorna apenas os 5 primeiros
+		encode_res = json.dumps(res, indent=2).encode('utf-8')             #transforma o dicionario em um json 
+		novoSock.send(encode_res)                                          #envia o resultado
 
-	except  FileNotFoundError as e:
+	except  FileNotFoundError as e:                                           #trata a inexistencia do arquivo no diretorio
 		novoSock.send("0")
 		break
 
